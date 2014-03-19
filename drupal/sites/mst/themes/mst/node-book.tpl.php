3cafbb7af413ca17987354aee1874536d251decc
Rodrigo Alencar and Vinicius Gomes <vinicius.vieira.gomes+alencar+gomes@gmail.com>
2014-03-19 14:09:59 -0300

    remove o diretorio drupal e config do novo site

--- drupal/sites/mst/themes/mst/node-book.tpl.php
+++
@@ -1,6 +0,0 @@
-  <div class="node book">
-    <div class="content"><?php print $content?></div>
-    <div class="taxonomy"><h3><?php print $terms?></h3></div>
-    <?php if ($links) { ?><div class="links">&raquo; <?php print $links?></div><?php }; ?>
-  </div>
-
--- drupal/sites/mst/tools/dump.sh
+++
@@ -1,8 +0,0 @@
-mysqldump -u root mst_novo blocks blocks_roles > mst-blocks.sql 
-mysqldump -u root mst_novo views_view views_display > mst-views.sql
-mysqldump -u root mst_novo boxes > mst-boxes.sql
-mysqldump -u root mst_novo imagecache_preset imagecache_action > mst-imagecache.sql
-mysqldump -u root mst_novo content_node_field content_node_field_instance content_type_product > mst-cck.sql
-# mysqldump -u root mst_novo menu_links > mst-menu.sql
-mysqldump -u root mst_novo uc_cart_products  uc_countries uc_order_admin_comments uc_order_comments uc_order_line_items uc_order_log uc_order_products  uc_order_statuses uc_orders uc_product_classes uc_product_features uc_products uc_store_footers uc_zones ca_predicates > mst-ubercart.sql
-mysqldump -u root mst_novo system > mst-system.sql
